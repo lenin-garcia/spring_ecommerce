a5b5b94ae3c82503f9b44e2feee732eeb2a51bb9
lenin <leninmaximo.lg@gmail.com>
2023-07-16 10:06:40 +0200

    creamos vista del detalle del producto

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -4,10 +4,13 @@ import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.repository.ProductoRepository;
 import com.curso.ecommerce.services.ProductoService;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,12 +21,21 @@ public class HomeController {
     @Autowired
     private ProductoService productoService;
     
+    private Logger log = LoggerFactory.getLogger(HomeController.class);
 
     //enviara los prodcutos a la vista desde la bbdd con el objeto Model
     @GetMapping("")
     public String home(Model model) {
         model.addAttribute("productos", productoService.finAll());// con esto enviamos a la vista desde el cont
         return "usuario/home";
+    }
+    
+    
+    //nos llevara del producto hasta la vista del detalle del producto
+    @GetMapping("/productohome/{id}")
+    public String productoHome (@PathVariable Integer id ){
+        log.info("Id enviado para mostrar detalle de producto{}",id);
+        return "usuario/productohome";
     }
 
 }
